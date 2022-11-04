99ea2000ef6462a16e3bfb7b292dc172b45778df
AlexDavisIII <alex.iii.davis@gmail.com>
2022-11-04 16:48:05 -0400

    Further Menu and responsiveness manipulation
    Changing the look of the button to fit the more conventional
    "hamburger menu".  Also modified media queries as well as position
    (and size) of the navigation menu options.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuToggle = document.querySelector('.navigation button');
 const menu = document.querySelector('.navigation ul');
 const navBar= document.querySelector('.navigation');
+/*const menuLine = document.querySelector('.line');*/
 
 menuToggle.addEventListener('click',function(){
     const open = JSON.parse(menuToggle.getAttribute('aria-expanded'));
@@ -15,4 +16,23 @@ menuToggle.addEventListener('click',function(){
         navBar.classList.remove('fullscreenNav');
         menuToggle.classList.remove('hideMe');
     }
+});
+
+
+
+menuToggle.addEventListener('mouseover',function(){
+    menuLine.target.style.color="red";
+    /*
+    const open = JSON.parse(menuToggle.getAttribute('aria-expanded'));
+    menuToggle.setAttribute('aria-expanded', !open);
+    menu.hidden = !menu.hidden;
+
+    if (!menu.hidden){
+        menu.querySelector('a').focus();
+        navBar.classList.add('fullscreenNav');
+        menuToggle.classList.add('hideMe');
+    }else {
+        navBar.classList.remove('fullscreenNav');
+        menuToggle.classList.remove('hideMe');
+    }*/
 });

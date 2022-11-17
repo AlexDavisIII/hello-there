9d9f1ad70c4d679f4b037696daae5ec8aa389616
Alex <alex.iii.davis@gmail.com>
2022-11-16 23:24:11 -0500

    Auto-close for hamburger button!
    Through use of an additional javascript event listener, we now
    have the ability to open the hamburger menu and allow it to close
    automatically close should a user on their desktop choose to widen
    their browser window while the menu is still opened. Took quite a bit
    of searching to get this one done, but we did it!

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const menuToggle = document.querySelector('.navigation button');
 const menu = document.querySelector('.navigation ul');
 const navBar= document.querySelector('.navigation');
-/*const menuLine = document.querySelector('.line');*/
-const contactButton = document.querySelector('.contactButton');
-const contactButtonLink=document.querySelector('.contactButton p');
+const mediaQuery = window.matchMedia('(min-width: 650px)');
+
+const mainNavigation = document.getElementById("navigation");
+const hamburger = document.getElementById("menuNavigationBtn");
+const menuList = document.getElementById("menuNavigationBtn")
+
 
 menuToggle.addEventListener('click',function(){
     const open = JSON.parse(menuToggle.getAttribute('aria-expanded'));
     menuToggle.setAttribute('aria-expanded', !open);
     menu.hidden = !menu.hidden;
+
 
     if (!menu.hidden){
         menu.querySelector('a').focus();
@@ -22,24 +26,15 @@ menuToggle.addEventListener('click',function(){
 
 
 
-contactButton.addEventListener('mouseover',function(){
-      contactButtonLink.target.style.color ="green";
-})
+function buttonDetailToggle(){
+    /*if the media query matches the min-width: 650, this toggles the hidden attribute
+    of the menu list (so when the hamburger menu reappears the menu stays hidden) and 
+    removes the fullscreenNav I made.*/
+    if(mediaQuery.matches){
+        mainNavigation.classList.remove("fullscreenNav");
+        menu.toggleAttribute("hidden");
+    }
+    
+}
 
-
-menuToggle.addEventListener('mouseover',function(){
-    menuLine.target.style.color="red";
-    /*
-    const open = JSON.parse(menuToggle.getAttribute('aria-expanded'));
-    menuToggle.setAttribute('aria-expanded', !open);
-    menu.hidden = !menu.hidden;
-
-    if (!menu.hidden){
-        menu.querySelector('a').focus();
-        navBar.classList.add('fullscreenNav');
-        menuToggle.classList.add('hideMe');
-    }else {
-        navBar.classList.remove('fullscreenNav');
-        menuToggle.classList.remove('hideMe');
-    }*/
-});
+mediaQuery.addEventListener('change', buttonDetailToggle);
